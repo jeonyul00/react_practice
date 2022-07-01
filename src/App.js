59a010c46e45cfb286986b13f6c57d657523906d
YUL JEON <90760450+jeonyul00@users.noreply.github.com>
2022-07-01 14:39:09 +0900

    update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Map from "./Pages/220619_Map";
 function App() {
   return (
     <>
+      {/* 아예 개념부터 다시하자 , 220701 */}
       <Map />
       <p>-------------------------------------</p>
       <Filter />
---
+++ src/Pages/220701_WhyReact.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// 왜 리액트를 배워야하는가?
+// 나는 이걸 답변하지 못했다 시발
+function WhyReact() {
+  return <div>220701_WhyReact</div>;
+}
+
+export default WhyReact;
