e624e29aeaa9abc10ca1b8726b98f7e3ff78fcc4
YUL JEON <90760450+jeonyul00@users.noreply.github.com>
2022-07-04 16:03:40 +0900

    update

--- src/Pages/javascript/220703_DocumentObject.js
+++ src/Pages/javascript/220703_DocumentObject.js
@@ -6,6 +6,7 @@ import React from "react";
 //  ㄴ html을 가리키는 객체
 // 요약 : 결국 브라우저가 읽는것은 html이다 그 html에서 javascript와 css가 호출되는거지
 //  ㄴ 근데 document는 html에 접근하는 오브젝트이므로 결론적으로 화면에 보이는 모든것에 접근할 수 있다
+// document 오브젝트 안에는 수많은 함수들이 있다 예를 들면 innerHTML , Autofocus etc
 
 console.log(document.title);
 // 이처럼 html에 접근 가능하고 정제? 가능
@@ -21,7 +22,23 @@ console.log(document.getElementsByClassName("className"));
 const html = document.getElementsByClassName("className");
 html[0].innerHTML = "이 밑으로 부터는 javascript영역";
 
-// document 오브젝트 안에는 수많은 함수들이 있다 예를 들면 innerHTML , Autofocus etc
+const selecter = document.querySelector("#title"); // 딱 선택해서 값을 반환하게 할 수 있다, 1개의 요소만 반환, 처음 만나는 하나만 , 보다 상세히 검색할 수 있다 : css처럼
+const selecterAll = document.querySelectorAll("h1"); // props를 가진 모든 요소 반환, array형
+
+console.log(selecter);
+console.log(selecterAll);
+
+// add event
+const handleClick = () => {
+  return console.log("event");
+};
+// or
+function handleClickE() {
+  return console.log("event!!");
+}
+//
+const e = document.querySelector("#title");
+e.addEventListener("click", handleClick); // addEventListener : 1번 index : 했을 시 , 2번 index : 호출
 
 function DocumentObject() {
   return (
