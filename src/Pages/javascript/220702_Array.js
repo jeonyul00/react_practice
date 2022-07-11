19324525036bb24b528b0c8fa7cfd8face285e35
YUL JEON <90760450+jeonyul00@users.noreply.github.com>
2022-07-11 15:32:37 +0900

    update

--- src/Pages/javascript/220702_Array.js
+++ src/Pages/javascript/220702_Array.js
@@ -26,6 +26,57 @@ daysOfWeek = daysOfWeek.slice(0, 2);
 // concat : 배열 끝에 새로운 요소 추가 : 실무에서 많이 쓰일것 같다
 daysOfWeek = daysOfWeek.concat("wed", "thu", "fri", "sat", "sun");
 
+// 배열 내장 함수 심화 ==================================================\
+const number = [1, 2, 3, 4, 5];
+// forEach : 반복문
+number.forEach(() => {
+  console.log(number);
+});
+
+// map : 배열안의 요소를 변환하고 싶을때 사용
+const nuwNum = number.map((n) => n * n);
+
+// filter : 특정 조건을 만족하는 요소를 찾아서 새로운 배열을 만든다
+const userFirstname = [
+  {
+    firstName: "jeon",
+  },
+  {
+    firstName: "jeon",
+  },
+  {
+    firstName: "choi",
+  },
+  {
+    firstName: "kim",
+  },
+  {
+    firstName: "lee",
+  },
+];
+
+const newFirstName = userFirstname.filter((v) => v.firstName === "jeon");
+console.log(newFirstName); // 특정 조건에 만족하는 요소만 나옴 : jeon만 나오겠지
+
+// splice , slice
+const nubers = [1, 2, 3, 4, 5, 6];
+// splice : 특정 항목 제거 할 때 사용
+nubers.splice(1, 2); // 1번 인덱스 부터 시작해서 2개 지우겠다, 그리고 이 지운값이 이 함수의 값임 이게 무슨말이냐면 그건 밑에
+const value = nubers.splice(1, 2); // value = [2,3]
+
+// slice : 배열을 잘라낼 때 사용 되는데 , 기존의 배열을 건들지 않는다
+const slice = nubers.slice(1, 2); // 1번 인데스부터 2번 인덱스까지 자르겠다 , 이 명령 후에도 기존의 배열 값은 변하지 않는다, splice는 잘라서 버리는데 비해 slice는 자르기만 하는거지
+
+// shift , pop , unshift
+// shift : 첫번째 원소를 배열에서 추출, 기존의 원소를 하나씩 밖으로 빼내주는거지
+// pop : 마지막 원소를 배열에서 추출 , 기존의 원소를 하나씩 밖으로 빼내주는거지
+// unshift : 0번 인덱스에 요소 추가 : push는 마지막 인덱스에 추가 되는데 비스무리 하네
+// concat : 더하기
+// join : 문자열로 더하기, 근데 요소들 사이에 뭘 넣을 수 있다
+
+// reduce : 배열이 주어졌을 때 베열 안의 값들로 어떤걸 연산해야 할 때 사용
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 function Array() {
   return (
     <>
--- src/Pages/javascript/220702_Object.js
+++ src/Pages/javascript/220702_Object.js
@@ -27,6 +27,46 @@ exObject[0].address = "seoul";
 // const는 상수인데 왜 업데이트가 가능하냐 : 프로퍼티를 바꿨지 오브젝트를 바꾼게 아니기 때문이다
 // error는 constant 전체를 하나의 값으로서 업데이트 하려고 할 때 발생한다
 
+// 비구조화 할당 , es6에 있는 문법으로 객체구조분해라고도 부름, 객체에서 특정값을 추출해내는것
+const ironMan = {
+  name: "토니스타크",
+  actor: "로버트 다우니 주니어",
+  alias: "아이언맨",
+  // 화살표 함수는 자기가 속해있는 this 명령이 안먹힘 -> 그래서 function을 씀
+  say: function () {
+    console.log(`i am ${this.alias}`);
+  },
+};
+
+// function print(hero) {
+//   // 객체 내부의 값을 빼와서 선언
+//   const { alias, name, actor } = hero;
+//   const text = `${alias}(${name}) 역할을 맡은 배우는 ${actor} 배우 입니다`;
+//   console.log(text);
+// }
+
+// 이런식으로 더 많이 씀
+function print({ alias, name, actor }) {
+  const text = `${alias}(${name}) 역할을 맡은 배우는 ${actor} 배우 입니다`;
+  console.log(text);
+}
+
+print(ironMan);
+
+// 객체 내에서 getter , setter 함수
+const car = {
+  name: "avante",
+  price: "230,000,000",
+  // get
+  getName() {
+    return this.name;
+  },
+  // set
+  setName(newName) {
+    this.name = newName;
+  },
+};
+
 function Object() {
   return (
     <>
